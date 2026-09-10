6b30de1f4d1e05771b9a7393a6714ff87da895d2
Brandon <blunacastro2003@gmail.com>
2026-09-10 11:26:50 -0300

    estructura inicial: fastapi, sqlalchemy, alembic

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+#le decimos al sistema que cargue las variables dentro de nuestro archivo env
+load_dotenv()
+
+#esta linea de codigo sirve para leer nuestra variable de entorno que es la que actualmente contiene nuestra coneccion a la base de datos 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+#basicamente creamos el puente entre nuestra base de datos y el programa 
+engine = create_engine(DATABASE_URL)
+
+# esto nos permite crear sesiones, las cuales son importantes al momento que interactuen en la base de datos 
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush= False,
+    bind = engine 
+)
+
+Base = declarative_base()
---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+#crea una app Fast API
+app = FastAPI()
+
+#cuando alguien haga una peticion get ejecuta nuestra funcion get
+@app.get("/health")
+def healt_check():
+    return {"status" :  "ok"}
