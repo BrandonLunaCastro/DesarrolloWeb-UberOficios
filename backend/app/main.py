ef53db3a5d5f6ffb1661eff2b6cf1a0a0ac31b56
Brandon <blunacastro2003@gmail.com>
2026-09-11 13:49:38 -0300

    agregado la configuracion del CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,23 @@ from fastapi import FastAPI
 # Importamos el router que contiene nuestros endpoints
 # de autenticación.
 from app.routes.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 #crea una app Fast API
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",   # Vite dev server
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Incluimos el router de autenticación dentro de nuestra aplicación.
 # Esto hace que FastAPI conozca los endpoints que definimos
